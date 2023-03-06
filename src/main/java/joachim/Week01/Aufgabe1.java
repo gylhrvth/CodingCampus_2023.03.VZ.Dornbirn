88991f5d4ff1f618b8a234ba9f111ad425d114da
Joachim <Joachim.Nachbaur@vol.at>
2023-03-06 13:07:47 +0100

    Joachim

--- src/main/java/joachim/Week01/Aufgabe1.java
+++ src/main/java/joachim/Week01/Aufgabe1.java
@@ -3,6 +3,19 @@ package joachim.Week01;
 public class Aufgabe1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("            |--|");
+        System.out.println("            |  |");
+        System.out.println("            |  |");
+        System.out.println("            |  |");
+        System.out.println(" \\-\\_______/  /");
+        System.out.println("  \\__________/");
+        System.out.println();
+        System.out.println("/--------------\\");
+        System.out.println("|   ________   |");
+        System.out.println("|  /        \\  |");
+        System.out.println("|  |        |  |");
+        System.out.println("|  \\________/  |");
+        System.out.println("\\______________/");
         System.out.println();
         System.out.println("     /--\\");
         System.out.println("    / /\\ \\");
@@ -10,5 +23,30 @@ public class Aufgabe1 {
         System.out.println("  / /    \\ \\");
         System.out.println(" /  ======  \\");
         System.out.println("/_/        \\_\\");
+        System.out.println();
+        System.out.println("/--------------\\");
+        System.out.println("|       _______/");
+        System.out.println("|      /");
+        System.out.println("|      \\_______");
+        System.out.println("|              \\");
+        System.out.println("\\______________/");
+        System.out.println();
+        System.out.println("|--|         |--|");
+        System.out.println("|  |_________|  |");
+        System.out.println("|               |");
+        System.out.println("|   _________   |");
+        System.out.println("|  |         |  |");
+        System.out.println("|__|         |__|");
+        System.out.println();
+        System.out.println("      |--|");
+        System.out.println("      |  |");
+        System.out.println("      |  |");
+        System.out.println("      |  |");
+        System.out.println("      |  |");
+        System.out.println("      |__|");
+
+
+
+
     }
 }
