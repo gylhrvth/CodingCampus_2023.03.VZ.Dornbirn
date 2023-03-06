e9afb357d73e156b91b6871da4f510149064be9f
Joachim <Joachim.Nachbaur@vol.at>
2023-03-06 15:33:16 +0100

    Funktionen ohne Max Value

--- src/main/java/joachim/Week01/Funktionen.java
+++ src/main/java/joachim/Week01/Funktionen.java
@@ -2,32 +2,58 @@ package joachim.Week01;
 
 public class Funktionen {
     public static void main(String[] args) {
+        int start = 0;
+        int end = 10;
 
-        public static void Zaehlenfor {
+        int altstart = 10;
+        int altend = -10;
 
-            System.out.println("Zaehlen for");
-            for (int i = 0; i <= 10; i++) {
-                System.out.println("i: " + i);
-            }
-            System.out.println("Zaehlen while");
-            int k = 0;
-            while (k <= 10) {
+        zahlenfor(start, end);
+        zahlenwhile(start, end);
+        zahlentrick(start, end);
+        zahlenrewind(altstart, altend);
 
-                System.out.println("k: " + k);
-                k += 1;
-            }
-            System.out.println("Zaehlen Trick");
-            for (int i = 0; i <= 10; i += 1) {
-                if (i % 2 < 1) {
-                    System.out.println(i);
-                }
-            }
-            System.out.println("Zaehlen rewind");
-            for (int j = 10; j >= -10; j = j - 1) {
-                System.out.println("j: " + j);
-            }
+    }
 
 
+    public static void zahlenfor(int start, int end) {
+
+        System.out.println("Zaehlen for");
+        for (int i = start; i <= end; i++) {
+            System.out.println("i: " + i);
         }
     }
+
+
+    public static void zahlenwhile(int start, int end) {
+        System.out.println("Zaehlen while");
+        int k = start;
+        while (k <= end) {
+
+            System.out.println("k: " + k);
+            k += 1;
+        }
+    }
+
+
+    public static void zahlentrick(int start, int end) {
+        System.out.println("Zaehlen Trick");
+        for (int i = start; i <= end; i += 1) {
+            if (i % 2 < 1) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+    public static void zahlenrewind(int altstart, int altend) {
+
+        System.out.println("Zaehlen rewind");
+        for (int j = altstart; j >= altend; j = j - 1) {
+            System.out.println("j: " + j);
+        }
+
+
+    }
+
 }
