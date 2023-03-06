47b91e160e7fc07fde9efa2ec8ad45487b734953
Gyula Horvath <gylhrvth@gmail.com>
2023-03-06 11:42:43 +0100

    Variable nund Kontrollstrukturen

---
+++ src/main/java/gyula/week01/ControlStructures.java
@@ -0,0 +1,24 @@
+package gyula.week01;
+
+public class ControlStructures {
+    public static void main(String[] args) {
+
+        // Sequenz
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+
+        // If statement
+        double weight = 5.0;
+        if (weight < 10.0) {
+            System.out.println("Klein");
+        } else {
+            System.out.println("Groß");
+        }
+
+        // Schleife
+        for (int i = 0; i < 10; i = i + 1){
+            System.out.println("i: " + i);
+        }
+    }
+}
