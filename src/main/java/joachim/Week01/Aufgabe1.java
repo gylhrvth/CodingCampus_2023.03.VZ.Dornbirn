35aef6053c86fa4a48ee26d6e9b16a2917f2cf6a
Joachim <Joachim.Nachbaur@vol.at>
2023-03-06 12:04:28 +0100

    Leerzeile Hinzugefügt

--- src/main/java/joachim/Week01/Aufgabe1.java
+++ src/main/java/joachim/Week01/Aufgabe1.java
@@ -3,6 +3,7 @@ package joachim.Week01;
 public class Aufgabe1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println();
         System.out.println("     /--\\");
         System.out.println("    / /\\ \\");
         System.out.println("   / /  \\ \\");
