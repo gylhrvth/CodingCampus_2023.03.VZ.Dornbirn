12d026c722d54d36e1ebe49235005f5db81a30f0
Beni <benjamin.herter@gmx.at>
2023-03-06 15:36:44 +0100

    Einfügen Funktionen

---
+++ src/main/java/benjamin/week01/AufgabeCount.java
@@ -0,0 +1,66 @@
+package benjamin.week01;
+
+public class AufgabeCount {
+
+    public static void main(String[] args) {
+        int maxValue = meinMax(10, -10);
+
+        aufgabe1(0, maxValue);         //count with for and with
+        aufgabe2(0, maxValue);         //count to 10 without 10
+        countwithtrick(0, maxValue);
+        rewind(maxValue, -10);
+    }
+    public static int meinMax(int a, int b) {
+        int result = b;
+        if (a > b) {
+            result = a;
+        }
+
+        return result;
+    }
+
+public static void aufgabe1(int start, int end){
+    System.out.println("Aufgabe 1");
+
+    System.out.println("Count with for");
+    for (int i = start; i <= end; i = i + 1) {
+        System.out.println(i);
+    }
+    System.out.println("Count with while");
+    int k = 0;
+    while (k <= 10){
+        System.out.println(k);
+        k = k + 1;
+    }
+
+}
+public static void aufgabe2(int start, int end){
+    System.out.println("Aufgabe 2");
+    for (int i = start; i < end; i = i + 1) {
+        System.out.println(i);
+    }
+}
+public static void countwithtrick(int start, int end){
+    System.out.println("Aufgabe Zaehlen mit Trick");
+
+
+    for (int i = start; i <= end; i = i + 1) {
+
+        if (i % 2 == 0)
+            System.out.println(i);
+    }
+
+    for (int i = 0; i <= 10; i = i + 2){
+        System.out.println(i);
+
+    }
+
+}
+public static void rewind(int start, int end) {
+    System.out.println("Aufgabe Rewind");
+    for (int i = start; i >= end; i = i - 1) {
+        System.out.println(i);
+    }
+}
+
+}
