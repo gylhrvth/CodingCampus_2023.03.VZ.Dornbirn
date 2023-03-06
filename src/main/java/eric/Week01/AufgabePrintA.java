a7f409772f821fbbaa10f9ce5adc2b9192a4606c
Eric G <eric.gangloff08@gmail.com>
2023-03-06 11:58:30 +0100

    Aufgabe: Print A

---
+++ src/main/java/eric/Week01/AufgabePrintA.java
@@ -0,0 +1,12 @@
+package eric.Week01;
+
+public class AufgabePrintA {
+    public static void main(String[] args) {
+        System.out.println("       /--\\        ");
+        System.out.println("      / /\\ \\      ");
+        System.out.println("     / /  \\ \\     ");
+        System.out.println("    / /    \\ \\    ");
+        System.out.println("   /  ======  \\    ");
+        System.out.println("  /_/        \\_\\  ");
+    }
+}
