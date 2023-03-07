2da48b6f85a64ba11c1c39c5cbd9b837c2174f87
ZaynHassan7 <zaynhassan9@gmail.com>
2023-03-07 09:12:32 +0100

    Mein erstes Programm

--- src/main/java/joachim/Week01/Funktionen.java
+++ src/main/java/joachim/Week01/Funktionen.java
@@ -3,7 +3,7 @@ package joachim.Week01;
 public class Funktionen {
     public static void main(String[] args) {
 
-        public static void Zaehlenfor {
+
 
             System.out.println("Zaehlen for");
             for (int i = 0; i <= 10; i++) {
@@ -28,6 +28,6 @@ public class Funktionen {
             }
 
 
-        }
+
     }
 }
