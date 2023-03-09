a9c303529fea404666ccb8a228ebfe3a2228a61c
mariechristine <m.koeninger@gmx.at>
2023-03-09 15:06:49 +0100

    Überarbeitung Triangle

--- src/main/java/mariechristine/ExercisesMethodsLoops1.java
+++ src/main/java/mariechristine/ExercisesMethodsLoops1.java
@@ -2,19 +2,20 @@ package mariechristine;
 
 public class ExercisesMethodsLoops1 {
     public static void main(String[] args) {
-        int text = 'X';
-        int count = 10;
 
 //        printChars("X ", 10);
 //        printSquare("A ", 10);
 //        printRect("B ", 3, 5);
-//        printTriangleBottomLeft("C ", 4);
-//        printTriangleTopLeft("D ", 5);
-//        printTriangleTopRight("E ", 5);
-        printEmptySquare("F ", 10);
-        printEmptySquareA("A", 3);
-        printSlash("X", 3, true );
-        printSlashA("Y ", 4, false);
+        printTriangleBottomLeft("C ", 4);
+        printTriangleTopLeft("D ", 5);
+        printTriangleTopRight("E ", 5);
+        printTriangleBottomRight("X ", 5);
+//        printEmptySquare("F ", 10);
+//        printEmptySquareA("A", 3);
+//        printSlash("X ", 3);printSlashA("Y ", 4);
+//        printTriangle("X ", 3);
+//        printSlashDown("X ", 10, true);
+//        printSlashDown("X ", 5, false);
     }
 
     public static void printChars(String text, int count) {
@@ -51,7 +52,7 @@ public class ExercisesMethodsLoops1 {
         System.out.println("Aufgabe: Print Trinagle 1");
         for (int i = 0; i < count; ++i) { // Anzahl der Zeile wird gedruckt
             for (int k = 0; k < i + 1; ++k) { // Buchstaben in einer Zeile schreiben
-                System.out.print(text);  // Schreiben eine Buchstabe
+                System.out.print(text);  // Schreibt ein Buchstabe
             }
             System.out.println(); // Zeilenumbruch am Ende der Zeile
         }
@@ -79,23 +80,25 @@ public class ExercisesMethodsLoops1 {
 
     public static void printTriangleTopRight(String text, int count) {
         System.out.println("Aufgabe: Print Triangle 3");
-        for (int z = 0; z < count; ++z) { //Anzahl der Zeile wird gedruckt
-            printCharsWOLineBreak("  ", z);
-            printCharsWOLineBreak(text, count - z);
+        for (int z = 0; z < count; ++z) { //Anzahl der Zeilen wird gedruckt
+            printCharsWOLineBreak("  ", z); // Beginnend bei Zeile 0 (Z1 kein Leerzeichen, Z2 1 Leerzeichen, etc
+            printCharsWOLineBreak(text, count - z); //definiert wie viel Buchstaben in einer Zeile sind
             System.out.println();
         }
     }
 
 
-    public static void printTriangleTopRightA(String text, int count) {
-        System.out.println("Aufgabe: Print Trinagle 3A");
-        for (int i = 0; i < count; ++i) {
-            for (int j = 0; j < count - 1; j++) {
-                System.out.println(text);
-            }
-            {
+    public static void printTriangleBottomRight(String text, int count) {
+        System.out.println("Aufgabe: Print Trinagle 4");
+        for (int i = 0; i < count; ++i) { // Anzahl der Zeilen wird gedruckt
+            printCharsWOLineBreak(" ", count - i); //Wie viel Leerzeichen pro Zeile
+            for (int j = 0; j < i + 1 ; j++) {
+                System.out.print(text);
 
             }
+
+
+            System.out.println(text);
         }
     }
 
@@ -127,13 +130,12 @@ public class ExercisesMethodsLoops1 {
         printCharsWOLineBreak(text, count);
         System.out.println();
 
+
         //Mitte
         for (int i = 0; i < count - 2; i++) {
             System.out.print(text);
             printCharsWOLineBreak(".", count - 2);
-            System.out.print(text);
-            System.out.println();
-
+            System.out.println(text);
         }
 
         //Lezte Zeile
@@ -143,16 +145,33 @@ public class ExercisesMethodsLoops1 {
 
     }
 
-    public static void printSlash(String text, int count, boolean direction) {
-        System.out.println("Aufgabe: Print Slash");
-        System.out.println(text);
-        boolean test = (count == 3);
-        System.out.println("  " + text);
-        System.out.println("    " + text);
-
+    public static void printSlashDown(String text, int count, boolean backslash) {
+        if (backslash == true) {
+            printSlash(text, count);
+        } else {
+            printSlashA(text, count);
+        }
     }
 
-    public static void printSlashA(String text, int count, boolean direction) {
+    public static void printSlash(String text, int count) {
+        System.out.println("Aufgabe: Print Slash");
+//        System.out.println(text);
+//        boolean test = (count == 3);
+//        System.out.println("  " + text);
+//        System.out.println("    " + text);
+
+
+        for (int i = 0; i < count; i++) {
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print(text);
+            }
+            {
+                System.out.println();
+            }
+        }
+    }
+
+    public static void printSlashA(String text, int count) {
         System.out.println("Aufgabe: Print SlashA");
         System.out.println("      " + text);
         System.out.println("    " + text);
@@ -160,6 +179,26 @@ public class ExercisesMethodsLoops1 {
         System.out.println(text);
     }
 
+    public static void printTriangle(String text, int count) {
+        System.out.println("Aufgabe: Print Triangle");
+        for (int i = 0; i < count; ++i) {
+            for (int k = 0; k < i + 1; ++k) {
+                System.out.print(text);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void printChristmasTree(String text, int count) {
+        System.out.println("Aufgabe: Print Christmastree");
+        for (int i = 0; i < count; ++i) {
+            printCharsWOLineBreak(" ", count - i);
+            printCharsWOLineBreak(text, i);
+
+            System.out.println(text);
+        }
+    }
 }
 
 
