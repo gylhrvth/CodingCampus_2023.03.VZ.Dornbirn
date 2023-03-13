51cb418f1500500d46b2155b47e1dca9f1498ebb
Beni <benjamin.herter@gmx.at>
2023-03-13 15:33:53 +0100

    Primitive Datentypen

--- src/main/java/benjamin/week01/PrimitiveDatentypen.java
+++ src/main/java/benjamin/week01/PrimitiveDatentypen.java
@@ -1,19 +1,22 @@
 package benjamin.week01;
 
+import java.util.Random;
+
 public class PrimitiveDatentypen {
 
     public static void main(String[] args) {
 
-        System.out.println("Aufgabe Summe");
+            System.out.println("Aufgabe Summe");
 
-        int summe = 0;
+            int summe = 0;
 
-        for (int i = 20; i <= 50; i++) {
-            summe = summe + i;
-        }
-        System.out.println("Summe ist: " + summe);
+            for (int i = 20; i <= 50; i++) {
+                summe = summe + i;
+            }
+            System.out.println("Summe ist: " + summe);
 
-        System.out.println();
+            System.out.println();
+
 
         System.out.println("Faktorial");
 
@@ -68,7 +71,7 @@ public class PrimitiveDatentypen {
         long a = 2;
         long b = 3;
         long c = 4;
-        for (int i = 1; i <= 2000000; i ++) {
+        for (int i = 1; i <= 2000000; i++) {
             pi1 += zeichen1 * 4.0 / (a * b * c);
             zeichen1 *= -1;
             a += 2;
@@ -78,10 +81,41 @@ public class PrimitiveDatentypen {
         System.out.println("Pi = " + pi1);
 
 
+        System.out.println("Geheime Reihe");  //Wurzel 2 , Diagonale
+
+        double wert = 1.0;
+        for (int i = 0; i < 100; i++) {
+            wert = wert / 2.0 + (1.0 / wert);
+        }
+        System.out.print("Wert = " + wert);
+
+        System.out.println();
+
+
+        System.out.println("Wurzelannäherung");
+
+        double random = new
+        Random().nextInt(10000) + 1;
+        System.out.println("Ergebnis " + random);
+
+            random = random / 2.0 + (1.0 / random);
+            random = random / 4;
+
+        System.out.print("Wert = " + random);
+
+        System.out.println();
     }
 
 
+
 }
+
+
+
+
+
+
+
 
 
 
---
+++ src/main/java/benjamin/week02/ConsoleInputNumber.java
@@ -0,0 +1,106 @@
+package benjamin.week02;
+
+import benjamin.week01.MethodenSchleifen;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleInputNumber {
+
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+
+        // stringEinlesen();
+        // zahlEinlesen1();
+       // zahlEinlesen2();
+
+
+        // String text = ConsoleInputText.readText("Welcher Buchstabe?");
+        // int size = readNumber3("Wieviel Zeilen sollen ausgegebn werden", 1, 10);
+
+        //benjamin.week01.MethodenSchleifen.printRhombus(text,size);
+
+    }
+
+    public static void stringEinlesen() {
+        String name = ConsoleInputText.readText("Wie ist lautet Name?");
+        System.out.println("Hallo " + name + "!");
+    }
+
+    public static int readNumber(String message) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(message);
+        int value = sc.nextInt();
+
+        return value;
+
+
+    }
+
+    public static void zahlEinlesen1() {
+
+        int number = readNumber("Welche Nummer?");
+        System.out.println("Deine Nummer ist " + number + " !");
+
+    }
+
+    public static int readnumber2(String message) {
+        Scanner sc = new Scanner(System.in);
+        int value = 0;
+        boolean numberIsValid = false;
+        while (!numberIsValid) {
+            System.out.println(message);
+            try {
+                value = sc.nextInt();
+                sc.nextLine();
+                numberIsValid = true;
+            } catch (InputMismatchException ime) {
+                System.out.println("Das ist keine Zahl!");
+                sc.nextLine();
+            }
+        }
+        return value;
+    }
+
+    public static void zahlEinlesen2() {
+
+        int number = readnumber2("Welche Zahl?");
+        System.out.println("Deine Zahl ist " + number + " !");
+
+    }
+
+    public static int readNumber3(String message, int minValue, int maxValue) {
+        Scanner sc = new Scanner(System.in);
+        int value = 0;
+        boolean numberIsValid = false;
+        while (!numberIsValid) {
+            System.out.println(message);
+            try {
+                String line = sc.nextLine();
+                value = Integer.valueOf(line);
+                //sc.nextLine();
+                if ((value >= minValue) && (value <= maxValue)) {
+                    numberIsValid = true;
+                } else {
+                    System.out.println("Die Zahl muss zwischen " + minValue + " und " + maxValue + " sein! ");
+
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Keine gültige Zahl.");
+                sc.nextLine();
+            }
+        }
+        return value;
+
+
+    }
+
+    public static void rateSpiel(){
+        int random = new Random()
+
+
+
+    }
+}
