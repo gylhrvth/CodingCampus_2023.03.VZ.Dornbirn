7fdae40cfa03cc94fb321c69625a69b54d6109d3
mariechristine <m.koeninger@gmx.at>
2023-03-14 15:02:56 +0100

    Riddle kommentiert

--- src/main/java/mariechristine/week2/Riddle.java
+++ src/main/java/mariechristine/week2/Riddle.java
@@ -1,5 +1,8 @@
 package mariechristine.week2;
 
+import gyula.week02.ConsoleInputNumber;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -8,12 +11,43 @@ public class Riddle {
 
     public static void main(String[] args) {
 
-        System.out.println("Willkommen beim super coolen Zahlen Ratespiel!\nErrate die Zahl zwischen 0 und 100!\nWas ist deine Zahl?");
-        int x = ran.nextInt(101);
-        System.out.println();
+        System.out.println("Willkommen beim super coolen Zahlen Ratespiel!\nErrate die Zahl zwischen 0 und 100!");
+        int goal = ran.nextInt(101); //Die Zielzahl entspricht der random number; diese ist kleiner als 101
+        int guess = -1; //Die erratene Zahl soll im Plusbereich sein, Minus wird ausgeschlossen
 
+        while (goal != guess){ //Das Ziel ist ungleich der erratenen Zahl
+            guess = myReadNumber("Was ist deine Zahl?", 0, 100);
+            if (guess < goal){ // Ist die erratene Zahl kleiner als das Ziel, dann "zu klein"
+                System.out.println("Es ist zu klein");
+            } else if (guess > goal) { //Ist die erratene Zahl größer als das Ziel, dann "zu groß"
+                System.out.println("Es ist zu groß");
+            }
+        }
+        System.out.println("Gratuliere! Du hast korrekt erraten.");
 
     }
 
+
+    public static int myReadNumber(String message, int min, int max){
+        int value = min - 1;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (value < min || value > max) {
+            System.out.println(message);
+            try {
+                value = sc.nextInt();
+                sc.nextLine();
+            } catch (InputMismatchException ime) {
+                sc.nextLine();
+                System.out.println("Mir wäre lieber ein Zahl!");
+            }
+        }
+
+
+        return  value;
+    }
 }
+
+
 
