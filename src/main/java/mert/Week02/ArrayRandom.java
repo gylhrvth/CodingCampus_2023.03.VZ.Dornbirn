c0586c87f135e4fda90d05702276f24aabe2cf79
duelg <duelger_mert@hotmail.com>
2023-03-16 15:33:43 +0100

    week02

--- src/main/java/mert/Week02/ArrayRandom.java
+++ src/main/java/mert/Week02/ArrayRandom.java
@@ -1,5 +1,7 @@
 package mert.Week02;
 
+import gyula.week02.ConsoleInputNumber;
+
 import java.util.Arrays;
 import java.util.Random;
 
@@ -8,19 +10,51 @@ public class ArrayRandom {
 
     public static void main(String[] args) {
 
-        int size = 10;
-        int[] array = new int[size];
+//        int size = ConsoleInputNumber.readNumber_v5("Wie viele Elemende sollen erstellt werden?", 1, 1000);
+//        int[] array = new int[size];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            int zufallszahl = rand.nextInt(101);
+//            array[i] = zufallszahl;
+//
+//        }
+//
+//        for (int name : array) {
+//            System.out.print(name + ", ");
+//        }
+//        System.out.println();
+//
+//        System.out.println("Ausgabe mit Arrays.toString()");
+//        System.out.println(Arrays.toString(array));
+//        System.out.println("Ausgabe mit fori Schleife");
+//
+//        array[2] = 54;
+//        array[5] = 54;
+//        array[10] = 54;
+//
+//        System.out.print("[");
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i] + ", ");
+//
+//
+//        }
+//        System.out.print("]");
 
-        for (int i = 0; i < array.length; i++) {
+    }
+
+    public static int[] crazyRange(int[] size) {
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
             int zufallszahl = rand.nextInt(101);
-            array[i] = zufallszahl;
+            numbers[i] = zufallszahl;
+
+            for (int asdf : numbers) {
+
+                System.out.println(asdf);
+            }
 
         }
-        System.out.println(Arrays.toString(array));
+return numbers;
 
-        for (int name : array ) {
-            int zufallszahl = rand.nextInt(101);
-            System.out.print(name + ", ");
-        }
     }
 }
