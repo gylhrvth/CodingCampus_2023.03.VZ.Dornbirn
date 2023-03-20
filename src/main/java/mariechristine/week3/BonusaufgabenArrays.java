53f1f7c800d83655b7e18f2f0111c973845f0583
mariechristine <m.koeninger@gmx.at>
2023-03-20 15:32:31 +0100

    2D Array begonnen

---
+++ src/main/java/mariechristine/week3/BonusaufgabenArrays.java
@@ -0,0 +1,21 @@
+package mariechristine.week3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BonusaufgabenArrays {
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        createRandom2D(100);
+
+    }
+
+    public static int[][] createRandom2D(int size) {
+        int[][] numbers = new int[size][];
+
+        for (int i = 0; i < numbers.length; i++) {
+        }
+        return numbers;
+    }
+}
