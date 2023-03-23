44082ae1a9d394df86822539358b29978cdce2d9
ossdraboon <ossdraboon@gmail.com>
2023-03-23 14:44:30 +0100

    week3 donnerstag Game of Life

--- src/main/java/sandro/week03/ConwaysGameOfLife.java
+++ src/main/java/sandro/week03/ConwaysGameOfLife.java
@@ -7,52 +7,46 @@ public class ConwaysGameOfLife {
         int dead = 0;
 
 
-        int[][] currentgen = GLEITER;
-        printField(GLEITER);
+        int[][] currentgen = lukas.week03.day4.ConwaysGameOfLife.SEGLER;
         System.out.println(".....");
 
-        for (int i = 0; i < 20; i++) {
+        while(true) {
+            int[][] newGen = new int[currentgen.length][currentgen[0].length];
 
+            for (int row = 0; row < currentgen.length; row++) {
+                for (int col = 0; col < currentgen[row].length; col++) {
+                    int aliveNeighbours = countAliveNeighbours(currentgen, row, col);
 
+                    int currentCell = currentgen[row][col];
 
-        int[][] newGen = new int[GLEITER.length][GLEITER[0].length];
-
-
-        for (int row = 0; row < currentgen.length; row++) {
-            for (int col = 0; col < currentgen[row].length; col++) {
-                int aliveNeighbours = countAliveNeighbours(newGen, row, col);
-
-                int currentCell = currentgen[row][col];
-
-                if (currentCell == 0) {
-                    if (aliveNeighbours == 3) {
-                        newGen[row][col] = 1;
-                    }
-                } else {
-                    if (aliveNeighbours < 2) {
-                        newGen[row][col] = 0;
-                    } else if (aliveNeighbours == 2 || aliveNeighbours == 3) {
-                        newGen[row][col] = 1;
-                    }
-                    if (aliveNeighbours > 3) {
-                        newGen[row][col] = 0;
+                    if (currentCell == 0) {
+                        if (aliveNeighbours == 3) {
+                            newGen[row][col] = 1;
+                        }
+                    } else {
+                        if (aliveNeighbours < 2) {
+                            newGen[row][col] = 0;
+                        } else if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                            newGen[row][col] = 1;
+                        }
+                        if (aliveNeighbours > 3) {
+                            newGen[row][col] = 0;
+                        }
                     }
                 }
-            }newGen = currentgen;
+            }
+            currentgen = newGen;
 
+            printField(currentgen);
+            System.out.println(".....");
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                //noop
+            }
+            clearScreen();
         }
-        printField(currentgen);
-        System.out.println(".....");
-
-    }
-
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            //noop
-        }
-        clearScreen();
 
 
     }
@@ -86,6 +80,9 @@ public class ConwaysGameOfLife {
         int aliveNeighbours = 0;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
                 int neighbourRow = row + i;
                 int neighbourCol = col + j;
 
