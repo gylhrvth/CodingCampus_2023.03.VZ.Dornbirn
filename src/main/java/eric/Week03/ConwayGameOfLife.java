1dd17d83697608192439b98f02926a68cc4bf11b
Eric G <eric.gangloff08@gmail.com>
2023-03-23 15:28:44 +0100

    idk

--- src/main/java/eric/Week03/ConwayGameOfLife.java
+++ src/main/java/eric/Week03/ConwayGameOfLife.java
@@ -4,44 +4,56 @@ import java.util.Arrays;
 
 public class ConwayGameOfLife {
     public static void main(String[] args) {
-        for (int i = 0; i < GLEITER.length; i++) {
-            for (int j = 0; j < GLEITER[0].length; j++) {
 
-            }
+        int[][] printGlider = lukas.week03.day4.ConwaysGameOfLife.SEGLER;
+        lukas.week03.day4.ConwaysGameOfLife.clearScreen();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            //noop
         }
 
+        for (int row = 0; row < printGlider.length; row++) {
+            for (int col = 0; col < printGlider[row].length; col++) {
+                int printRulesOfLife = rulesOfLife(printGlider, row, col);
+                int printFieldValue = fieldValue(printGlider, row, col);
+                System.out.println(printRulesOfLife + printFieldValue);
+            }
+        }
     }
 
-    public static int fieldValue(int[][] field, int col, int row) {
+
+    public static int fieldValue(int[][] field, int row, int col) {
         if (row < 0) {
             row = field.length - 1;
-        } else if (row >= field[0].length) {
+        } else if (row >= field.length) {
             row = 0;
-
-
-            if (col < 0) {
-                col = field[0].length;
-            } else if (col >= field[0].length) {
-                col = 0;
-            }
         }
-        return field[row][col];
+
+
+        if (col < 0) {
+            col = field[0].length;
+        } else if (col >= field[0].length) {
+            col = 0;
+        }
+        return field.length;
     }
 
-    public static int rulesOfLife(int[][] field, int col, int row) {
+    public static int rulesOfLife(int[][] field, int row, int col) {
 
         int currentField = field[row][col];
 
         int livingNeighbours = 0;
 
-        livingNeighbours += field[row][col - 1];
-        livingNeighbours += field[row - 1][col];
-        livingNeighbours += field[row][col + 1];
-        livingNeighbours += field[row + 1][col];
-        livingNeighbours += field[row - 1][col - 1];
-        livingNeighbours += field[row + 1][col - 1];
-        livingNeighbours += field[row - 1][col + 1];
-        livingNeighbours += field[row + 1][col + 1];
+        livingNeighbours += fieldValue(field, row, col - 1);
+        livingNeighbours += fieldValue(field, row - 1, col);
+        livingNeighbours += fieldValue(field, row, col + 1);
+        livingNeighbours += fieldValue(field, row + 1, col);
+        livingNeighbours += fieldValue(field, row - 1, col - 1);
+        livingNeighbours += fieldValue(field, row + 1, col - 1);
+        livingNeighbours += fieldValue(field, row - 1, col + 1);
+        livingNeighbours += fieldValue(field, row + 1, col + 1);
 
         int nextField = 0;
 
@@ -50,33 +62,14 @@ public class ConwayGameOfLife {
                 nextField = 1;
             }
         } else {
-              if (livingNeighbours < 2){
-                  nextField = 0;
-              } else if (livingNeighbours == 2 || livingNeighbours == 3) {
-                  nextField = 1;
-              } else if (livingNeighbours > 3) {
-                  nextField = 0;
-              }
+            if (livingNeighbours < 2) {
+                nextField = 0;
+            } else if (livingNeighbours == 2 || livingNeighbours == 3) {
+                nextField = 1;
+            } else if (livingNeighbours > 3) {
+                nextField = 0;
+            }
         }
         return nextField;
-    }
-
-    public static final int[][] GLEITER = {
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-            {0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    };
-
-
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
     }
 }
