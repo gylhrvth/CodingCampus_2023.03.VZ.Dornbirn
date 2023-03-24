778deb7bb39ab36e82b0ffbe5628d764cfa32ae2
Eric G <eric.gangloff08@gmail.com>
2023-03-24 09:06:49 +0100

    Copy Paste für hassan

--- src/main/java/eric/Week03/ConwayGameOfLife.java
+++ src/main/java/eric/Week03/ConwayGameOfLife.java
@@ -1,25 +1,34 @@
 package eric.Week03;
 
+import lukas.week03.day4.Colors;
+
 import java.util.Arrays;
 
 public class ConwayGameOfLife {
     public static void main(String[] args) {
+        int[][] printConwaysOfLife = lukas.week03.day4.ConwaysGameOfLife.GLEITER;
 
-        int[][] printGlider = lukas.week03.day4.ConwaysGameOfLife.SEGLER;
-        lukas.week03.day4.ConwaysGameOfLife.clearScreen();
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            //noop
-        }
+        while(true) {
+            int[][] newField = new int[printConwaysOfLife.length][printConwaysOfLife[0].length];
 
-        for (int row = 0; row < printGlider.length; row++) {
-            for (int col = 0; col < printGlider[row].length; col++) {
-                int printRulesOfLife = rulesOfLife(printGlider, row, col);
-                int printFieldValue = fieldValue(printGlider, row, col);
-                System.out.println(printRulesOfLife + printFieldValue);
+
+            for (int row = 0; row < printConwaysOfLife.length; row++) {
+                for (int col = 0; col < printConwaysOfLife[row].length; col++) {
+                    int printRulesOfLife = rulesOfLife(printConwaysOfLife, row, col);
+                    newField[row][col] = printRulesOfLife;
+                }
             }
+
+            printConwaysOfLife = newField;
+            printField(printConwaysOfLife);
+
+            try {
+                Thread.sleep(250);
+            } catch(InterruptedException exc) {
+                //noop
+            }
+            lukas.week03.day4.ConwaysGameOfLife.clearScreen();
         }
     }
 
@@ -33,11 +42,11 @@ public class ConwayGameOfLife {
 
 
         if (col < 0) {
-            col = field[0].length;
+            col = field[0].length - 1;
         } else if (col >= field[0].length) {
             col = 0;
         }
-        return field.length;
+        return field[row][col];
     }
 
     public static int rulesOfLife(int[][] field, int row, int col) {
@@ -71,5 +80,19 @@ public class ConwayGameOfLife {
             }
         }
         return nextField;
+    }
+
+    public static void printField(int[][] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                int field = arr[row][col];
+                if (field == 0) {
+                    System.out.print(Colors.COLORS[1] + field + " " + Colors.RESET);
+                } else {
+                    System.out.print(Colors.COLORS[2] + field + " " + Colors.RESET);
+                }
+            }
+            System.out.println();
+        }
     }
 }
