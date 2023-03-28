b88d0806e3986c915a89c5b4af4991554035966c
mariechristine <m.koeninger@gmx.at>
2023-03-28 10:04:23 +0200

    Bubblesort String Exercises begonnen

--- src/main/java/mariechristine/week4/StringExercises.java
+++ src/main/java/mariechristine/week4/StringExercises.java
@@ -6,15 +6,51 @@ public class StringExercises {
     public static void main(String[] args) {
 
         String[] names = new String[]{"Joachim", "Mert", "Eric", "Marie-Christine", "Benjamin", "Sandro", "Aygün", "Hassan", "Svitlana", "Lukas", "Gyula"};
-        bubblesort(names);
-        System.out.println(Arrays.toString(names));
+
+        bubblesortAscending(names);
+        System.out.println("String Bubblesort Ascending:\n" + Arrays.toString(names) + "\n");
+
+        bubblesortDescending(names);
+        System.out.println("String Bubblesort Descending:\n" + Arrays.toString(names) + "\n");
+
+        bubblesortLexAsc(names);
+        System.out.println("String Bubblesort Lexicographically Ascending:\n" );
+
 
     }
 
-    public static void bubblesort(String[] names) {
+    public static void bubblesortAscending(String[] names) {
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < names.length - i - 1; j++) {
+                if (names[j].length() > names[j + 1].length()) {
+                    String temp = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp;
+                }
+            }
+        }
+    }
 
+    public static void bubblesortDescending(String[] names) {
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names.length - i - 1; j++) {
+                if (names[j].length() < names[j + 1].length()) {
+                    String temp = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void bubblesortLexAsc(String[] names) {
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names.length - i - 1; j++) {
+                if (names[j].length() > names[j + 1].length()) {
+                    String temp = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp;
+                }
             }
         }
     }
