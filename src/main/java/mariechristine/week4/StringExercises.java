161a01af87c09aeae766eb7d2e5999d1dad5a2ee
mariechristine <m.koeninger@gmx.at>
2023-03-28 11:43:47 +0200

    Aufgabe Reverse

--- src/main/java/mariechristine/week4/StringExercises.java
+++ src/main/java/mariechristine/week4/StringExercises.java
@@ -10,16 +10,21 @@ public class StringExercises {
         String[] names = new String[]{"Joachim", "Mert", "Eric", "Ömer", "Marie-Christine", "Benjamin", "Sandro", "Aygün", "Hassan", "Svitlana", "Lukas", "Gyula"};
 
         bubblesortAscending(names);
-        System.out.println("String Bubblesort Ascending:\n" + Arrays.toString(names) + "\n");
+        System.out.println("String Bubblesort Ascending without Collator:\n" + Arrays.toString(names) + "\n");
 
         bubblesortDescending(names);
-        System.out.println("String Bubblesort Descending:\n" + Arrays.toString(names) + "\n");
+        System.out.println("String Bubblesort Descending without Collator:\n" + Arrays.toString(names) + "\n");
 
         bubblesortLexAsc(names);
-        System.out.println("String Bubblesort Lexicographically Ascending:\n" + Arrays.toString(names) + "\n");
+        System.out.println("String Bubblesort Lexicographically Ascending with Collator:\n" + Arrays.toString(names) + "\n");
 
         bubblesortLexDesc(names);
-        System.out.println("String Bubblesort Lexicographically Descending:\n" + Arrays.toString(names) + "\n");
+        System.out.println("String Bubblesort Lexicographically Descending with Collator:\n" + Arrays.toString(names) + "\n");
+
+        bubblesortExtended(names, true);
+        System.out.println("Bubblesort String Extended (ascending) with Collator:\n" + Arrays.toString(names) + "\n");
+        bubblesortExtended(names, false);
+        System.out.println("Bubblesort String Extended (descending) with Collator:\n" + Arrays.toString(names) + "\n");
 
     }
 
@@ -66,12 +71,21 @@ public class StringExercises {
         for (int i = 0; i < names.length; i++) {
             for (int j = 0; j < names.length - i - 1; j++) {
 //                if (names[j].compareTo(names[j + 1]) < 0) {
-                    if (deCollator.compare(names[j], names[j + 1]) < 0) {
-                        String temp = names[j];
-                        names[j] = names[j + 1];
-                        names[j + 1] = temp;
-                    }
+                if (deCollator.compare(names[j], names[j + 1]) < 0) {
+                    String temp = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp;
                 }
             }
         }
     }
+
+    public static void bubblesortExtended(String[] names, boolean stringUp) {
+        if (!stringUp) {
+            bubblesortLexDesc(names);
+        } else {
+            bubblesortLexAsc(names);
+        }
+    }
+}
+
