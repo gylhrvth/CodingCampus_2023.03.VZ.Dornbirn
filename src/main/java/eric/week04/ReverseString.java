081dff7fc20673a69516ad7dde79cf52ce69fad7
Eric G <eric.gangloff08@gmail.com>
2023-03-28 13:26:51 +0200

    update

--- src/main/java/eric/week04/ReverseString.java
+++ src/main/java/eric/week04/ReverseString.java
@@ -1,26 +1,83 @@
 package eric.week04;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ReverseString {
 
     public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String s = " ";
-        String scan = new String(s.toCharArray());
+        String s = "Hello World";
+
+        System.out.println("Bitte gib ein Wort ein!");
+        String text = sc.nextLine();
+
+        System.out.println();
+        System.out.println("-------------------");
+        System.out.println();
+
+        char[] charArray = text.toCharArray();
+        reverse(charArray);
+        String reversed = new String(charArray);
+
+        System.out.println("Das Wort umgedreht:" + reversed);
+
+        System.out.println();
+        System.out.println("-------------------");
+        System.out.println();
+
+
+        char[] randomizeChar = text.toCharArray();
+        randomize(randomizeChar);
+        String randomizer = new String(randomizeChar);
+
+        System.out.println("Das Wort Zufällig ausgegeben: " + randomizer);
+
+        System.out.println();
+        System.out.println("-------------------");
+        System.out.println();
+
     }
 
 
+    public static void randomize(char[] charArray) {
+        int i = 0;
+        int j = charArray.length / 2;
+        while (i < j) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
 
-
-
-    public static void eingabeScanner(char[] scan){
-        System.out.println("Bitte gib ein Wort oder mehrere Zahlen deiner Wahl ein.");
-        try{
-            sc.nextInt();
-        } catch (InputMismatchException ime){
-            sc.nextLine();
+            i++;
+            j--;
         }
     }
+
+
+//    public static void randomize(char[] charArray) {
+//        for (int i = 0; i < charArray.length; i++) {
+//            for (int j = charArray.length - 1; j - 1 > i; j--) {
+//                if (j > i) {
+//                    char temp = charArray[i];
+//                    charArray[i] = charArray[j];
+//                    charArray[j] = temp;
+//                }
+//            }
+//        }
+//    }
+
+    public static void reverse(char[] charArray) {
+        int i = 0;
+        int j = charArray.length - 1;
+        while (j > i) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+
+
 }
