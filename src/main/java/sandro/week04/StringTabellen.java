e1e3aeac26991bd57303910a2c4ed25bcf7858ec
ossdraboon <ossdraboon@gmail.com>
2023-03-28 14:59:02 +0200

    Strings

---
+++ src/main/java/sandro/week04/StringTabellen.java
@@ -0,0 +1,14 @@
+package sandro.week04;
+
+public class StringTabellen {
+    public static void main(String[] args) {
+
+        String[] firstName = {"Alfonso", "Beatrix-Eleonor", "Cecil", "Daniel", "Elmar"};
+        String[] lastName = {"Klein", "Kinderdorfer", "Al Elmenar", "Schmidt", "Simma"};
+        int[] age = {40, 78, 5, 18, 81};
+        String[] place = {"Wien", "Schwarzach", "Wiener Neudorf", "Sankt Pölten", "Sankt Pölten"};
+        float[] distanceFromCapital = {0f, 654.4f, 12.457634366f, 120.0f, 119.9999f};
+
+
+    }
+}
