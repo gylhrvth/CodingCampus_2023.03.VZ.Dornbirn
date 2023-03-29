66f1448b1473e6c462635591d8036e4808ee6b9c
mariechristine <m.koeninger@gmx.at>
2023-03-29 11:30:13 +0200

    Buchstabenzählen begonnen

---
+++ src/main/java/mariechristine/week4/BuchstabenZaehlen.java
@@ -0,0 +1,18 @@
+package mariechristine.week4;
+
+import java.util.Arrays;
+
+public class BuchstabenZaehlen {
+    public static void main(String[] args) {
+        String simpleText = data.Texts.getSimpleText();
+        System.out.println(simpleText);
+
+        textLength(simpleText);
+
+    }
+
+    public static void textLength(String simpleText) { //Zerlegt den gesamten Text in einzelne Charakter
+        char [] charArray = simpleText.toCharArray();
+
+    }
+}
