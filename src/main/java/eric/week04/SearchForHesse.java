fb79031ea7b5296b6518591182fbe71c997a826e
Eric G <eric.gangloff08@gmail.com>
2023-03-29 08:31:35 +0200

    update

--- src/main/java/eric/week04/SearchForHesse.java
+++ src/main/java/eric/week04/SearchForHesse.java
@@ -4,20 +4,20 @@ import data.Texts;
 
 public class SearchForHesse {
     public static void main(String[] args) {
-        String input = Texts.HESSE;
-        System.out.println("Hessen ist im Text: ");
-        findHesse(input);
+        System.out.print("Hesse kommt im Text: ");
+        findHesse();
     }
 
-    public static void findHesse(String input){
+    public static void findHesse() {
+        String input = Texts.HESSE;
+        String searchFor = "Hesse";
         int count = 0;
-        int lastIndex = 0;
-        while (lastIndex != -1){
-            lastIndex = input.indexOf("Hessen", lastIndex);
-            if (lastIndex != -1){
-                count++;
-                lastIndex += "Hessen".length();
-            }
+        int lastIndex = input.indexOf(searchFor);
+        while (lastIndex != -1) {
+            count++;
+            System.out.println("Hesse wurde im text an Stelle: " + lastIndex + " gefunden!");
+            lastIndex = input.indexOf(searchFor, lastIndex + 1);
         }
+        System.out.println("Das Wort Hesse kommt im Text: " + count + " mal vor!");
     }
 }
