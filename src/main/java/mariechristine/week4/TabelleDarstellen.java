a674db126fbed1af813f9d0abf26000284d4117f
mariechristine <m.koeninger@gmx.at>
2023-03-30 13:21:01 +0200

    Email Regex aka. Regbert fertig

--- src/main/java/mariechristine/week4/TabelleDarstellen.java
+++ src/main/java/mariechristine/week4/TabelleDarstellen.java
@@ -1,12 +1,29 @@
 package mariechristine.week4;
 
+import java.util.Arrays;
+
 public class TabelleDarstellen {
 
     public static void main(String[] args) {
 
         int[][] chart = new int[7][7];
         printChart(chart);
-    }
+
+        String[] firstName = {"Alfonso", "Beatrix-Eleonor", "Cecil", "Daniel", "Elmar"};
+        String[] lastName = {"Klein", "Kinderdorfer", "Al Elmenar", "Schmidt", "Simma"};
+        String[][] names = {{"Alfonso", "Beatrix-Eleonor", "Cecil", "Daniel", "Elmar"},
+                            {"Klein", "Kinderdorfer", "Al Elmenar", "Schmidt", "Simma"}};
+        System.out.println(names);
+
+/*        for (int i = 0; i < firstName.length; i++) {
+
+            {
+                System.out.println(Arrays.toString(firstName));
+
+            }
+            
+*/        }
+
 
     public static void printChart(int[][] chart) {
         for (int col = 0; col < chart.length; col++) {
