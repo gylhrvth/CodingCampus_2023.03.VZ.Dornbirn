83470434e3d6f685b6f9d15bf0f3634029de2820
mariechristine <m.koeninger@gmx.at>
2023-03-31 12:21:51 +0200

    Hesse

--- src/main/java/mariechristine/week4/Hesse.java
+++ src/main/java/mariechristine/week4/Hesse.java
@@ -10,5 +10,33 @@ public class Hesse {
         String text = Texts.HESSE;
         System.out.println(text);
 
+        String wantedWord = "Hesse";
+        System.out.println(text.indexOf(wantedWord));
+        int index = text.indexOf(wantedWord, text.indexOf(wantedWord) + 1);
+        System.out.println(index);
+
+    }
+
+//        characterNumber(text);
+
+
+    public static void characterNumber(String text) {
+        char[] charArray = text.toCharArray();
+        int[] charToCount = new int[7000];
+
+        for (int i = 0; i < text.length(); i++) {
+            char index = text.charAt(i);
+            ++charToCount[index];
+
+        }
+    }
+
+    public static void countStrings(String text, String wantedWord, int index) {
+        for (int i = 0; i < text.length(); i++) {
+
+
+        }
     }
 }
+
+
