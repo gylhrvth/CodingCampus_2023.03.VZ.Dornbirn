b20c19e370017dabc89bb8e99aacf75424e51880
duelg <duelger_mert@hotmail.com>
2023-03-31 11:19:56 +0200

    String Beispiele

---
+++ src/main/java/mert/Week04/SubString.java
@@ -0,0 +1,10 @@
+package mert.Week04;
+
+public class SubString {
+    public static void main(String[] args) {
+
+        String a = "Wort 1";
+        String b = "Wort 2";
+        System.out.println(a.equals(b));
+    }
+}
