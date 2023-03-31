e4b7609ad782c7ec4593747074ed70fb52c6d7c9
duelg <duelger_mert@hotmail.com>
2023-03-31 12:22:00 +0200

    String Beispiele

--- src/main/java/mert/Week04/SubString.java
+++ src/main/java/mert/Week04/SubString.java
@@ -1,10 +1,21 @@
 package mert.Week04;
 
+import java.util.Arrays;
+
 public class SubString {
     public static void main(String[] args) {
 
-        String a = "Wort 1";
-        String b = "Wort 2";
-        System.out.println(a.equals(b));
+        String a = "Hallo wie gehts dir? Ich bin ein Zugfahrer";
+        String b = "Mir gehts gut. Zugfahren ist schön";
+
+        String[] arr1 = a.split(" ");
+        String[] arr2 = b.split(" ");
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+
+        System.out.println(arr1.equals(arr2));
+
+
     }
 }
