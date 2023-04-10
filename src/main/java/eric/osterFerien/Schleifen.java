ebcf7c7beceac11f2d18a470dd59e93d22b2e79b
Ric0801 <eric.gangloff08@gmail.com>
2023-04-10 12:25:58 +0200

    Montag

--- src/main/java/eric/osterFerien/Schleifen.java
+++ src/main/java/eric/osterFerien/Schleifen.java
@@ -1,7 +1,34 @@
-//package eric.osterFerien;
-//
-//public class Schleifen {
-//    public static void main(String[] args) {
-//
-//    }
-//}
+package eric.osterFerien;
+
+
+
+public class Schleifen {
+    public static void main(String[] args) {
+        System.out.println("Aufgabe 1");
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("---");
+
+        System.out.println("Aufgabe 2");
+        for (int i = -30; i <= 30; i++) {
+            if (i % 3 == 0)
+                System.out.println(i);
+        }
+
+        System.out.println("---");
+
+        System.out.println("Aufgabe 3");
+        int count = 0;
+        while (count <= 10) {
+            System.out.println(count);
+            if (count > 5) {
+                System.out.println();
+                System.out.println();
+            }
+            count++;
+        }
+    }
+}
