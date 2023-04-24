d6cf4f144d7eb428c794b94ed0a8b7925e5c84ac
ZaynHassan7 <zaynhassan9@gmail.com>
2023-04-24 15:32:00 +0200

    fuuuuyaahhhhhh

---
+++ src/main/java/hassan/week08/PersonExc/PersonsMain.java
@@ -0,0 +1,8 @@
+package hassan.week08.PersonExc;
+
+public class PersonsMain {
+    public static void main(String[] args) {
+        Persons info = new Persons("Hassan",20,187,75.0);
+        System.out.println(info);
+    }
+}
