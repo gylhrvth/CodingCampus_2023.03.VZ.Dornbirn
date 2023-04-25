446f18d4045d5e5b828dea9fb1fb15542cdf22e3
ossdraboon <ossdraboon@gmail.com>
2023-04-25 15:38:46 +0200

    week 08 ClassData

--- src/main/java/sandro/week08/photo/MemoryCard.java
+++ src/main/java/sandro/week08/photo/MemoryCard.java
@@ -4,20 +4,33 @@ public class MemoryCard {
     private String model;
     private int capacity;
     private int usedMB;
+    private int pics;
 
-    public MemoryCard(String model, int capacity, int usedMB){
+    public MemoryCard(String model, int capacity, int pics, int usedMB){
         this.model = model;
         this.capacity = capacity;
+        this.pics = pics;
         this.usedMB = usedMB;
     }
 
+    public void deleteMemory (){
+        usedMB = 0;
+        pics = 0;
+    }
 
-    public void saveData(int size){
+
+    public boolean saveData(int size){
+        if (size > capacity - usedMB){
+            System.out.println("!!! MEMORY FULL !!!");
+            return false;
+        }
         usedMB += size;
+        ++pics;
+        return true;
     }
 
     @Override
     public String toString() {
-        return "MemoryCard " + model + " with " + usedMB + "/" + capacity + " MB used/capacity";
+        return "MemoryCard " + model + " with " + usedMB + "/" + capacity + " MB used/capacity " + pics + " images";
     }
 }
--- src/main/java/sandro/week08/photo/TestFunktionPhotoCam.java
+++ src/main/java/sandro/week08/photo/TestFunktionPhotoCam.java
@@ -1,18 +1,27 @@
 package sandro.week08.photo;
 
+import sandro.week08.digitalCamera.DigiCam;
+
 import java.util.Random;
 
 public class TestFunktionPhotoCam {
    static Random rand = new Random();
     public static void main(String[] args) {
 
-        MemoryCard card1 = new MemoryCard("Kingston 1GB", 1000, 100);
-        MemoryCard card2 = new MemoryCard("Kingston 512MB", 512, 400);
+
+
+
+        MemoryCard card1 = new MemoryCard("Kingston 1GB", 1000,100, 990);
+        MemoryCard card2 = new MemoryCard("Kingston 512MB", 512, 20, 250);
 
         Objective obj1 = new Objective("Premium", 135);
         Objective obj2 = new Objective("Standart", 35);
 
-        PhotoCam2 nicon2 = new PhotoCam2("nikon","Coolpix P1000",48,0.3);
+        PhotoCam2 nicon2 = new PhotoCam2("nikon","Coolpix P1000",48,30);
+
+
+
+
         System.out.println("...insert card 2");
         nicon2.insertMemoryCard(card2);
         System.out.println("...insert card 1");
@@ -30,16 +39,39 @@ public class TestFunktionPhotoCam {
         nicon2.mountObjective(obj2);
         nicon2.takePhoto();
         System.out.println(nicon2);
+        nicon2.removeMemoryCard();
+
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------------");
 
 
 
-
-        PhotoCam2 sony = new PhotoCam2("Sony", "Alpha 7 II", 24, 0.3);
+        PhotoCam2 sony = new PhotoCam2("Sony", "Alpha 7 II", 24, 30);
+        sony.insertMemoryCard(card1);
+        sony.removeMemoryCard();
         sony.insertMemoryCard(card2);
+        sony.mountObjective(obj2);
+        System.out.println(sony);
+        for (int i = 0; i < 20; i++) {
+            sony.takePhoto();
+        }
+        System.out.println(sony);
+        sony.removeMemoryCard();
+        sony.insertMemoryCard(card1);
+        sony.deleteMem();
+        for (int i = 0; i < 120; i++) {
+            sony.takePhoto();
+        }
+        System.out.println(sony);
+        sony.deleteMem();
+        System.out.println(sony);
 
 
 
-
+//
+//        int testVarialbe = 30;
+//        System.out.printf("TestVariable: %.2f\n", testVarialbe*0.01);
 
 
 
