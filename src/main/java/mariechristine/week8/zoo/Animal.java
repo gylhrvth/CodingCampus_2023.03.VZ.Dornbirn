f628fe3bf685e7b7de2758eceb78aacc7a52ce92
mariechristine <m.koeninger@gmx.at>
2023-05-05 11:35:17 +0200

    Vet

--- src/main/java/mariechristine/week8/zoo/Animal.java
+++ src/main/java/mariechristine/week8/zoo/Animal.java
@@ -1,29 +1,76 @@
 package mariechristine.week8.zoo;
 
+import lukas.week03.day4.Colors;
+
 public class Animal {
     private String species;
     private String animalName;
+    private boolean wasFed = true;
+    private int maxHealth;
+    private int actualHealth;
+    private int biteForce;
 
-    public Animal(String animalName, String species) {
+
+
+    public Animal(String animalName, String species, int maxHealth, int biteForce) {
         this.species = species;
         this.animalName = animalName;
-
+        this.maxHealth = maxHealth;
+        this.actualHealth = maxHealth;
+        this.biteForce = biteForce;
     }
 
     public String getAnimalName() {
         return animalName;
     }
 
+    public boolean wasFed() {
+        return wasFed;
+    }
+
+    public void setFed(boolean wasFed) {
+        this.wasFed = wasFed;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public boolean isAlive(){
+        return actualHealth > 0;
+    }
+
+    public void bitesAnimal(Animal otherAnimal) {
+        otherAnimal.actualHealth -= biteForce;
+        System.out.print(animalName + "["+ actualHealth + "/"+ maxHealth + "]" + Colors.COLORS[1] + " bites " + Colors.RESET + otherAnimal.animalName);
+        if (otherAnimal.actualHealth > 0) {
+            System.out.println("[health: " + otherAnimal.actualHealth + "/"+ maxHealth + "]");
+        } else {
+            System.out.println(". " + otherAnimal + " R.I.P.");
+        }
+    }
+
     public String getSpecies() {
         return species;
     }
 
-    public void printStructure(){
+    public void printStructure() {
         System.out.println("│        ├── " + animalName + ", " + species);
     }
 
     @Override
     public String toString() {
         return animalName + ", " + species;
+    }
+
+    public void simulate(
+
+    ) {
+
+
     }
 }
