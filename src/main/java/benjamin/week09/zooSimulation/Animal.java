67c84b8caa98c15dcc3e65f1604118171cb0a218
Beni <benjamin.herter@gmx.at>
2023-05-08 08:31:38 +0200

    Zoo

--- src/main/java/benjamin/week09/zooSimulation/Animal.java
+++ src/main/java/benjamin/week09/zooSimulation/Animal.java
@@ -1,19 +1,59 @@
 package benjamin.week09.zooSimulation;
 
+import lukas.week03.day4.Colors;
+
 import java.util.HashMap;
+import java.util.Random;
 
 public class Animal {
+    private static Random rand = new Random();
     private String name;
     private String species;
     private Food food;
     private double amount;
+    private boolean isfeeded;
+    private int health;
 
+    private int currentHealth;
+    private int bite;
 
-    public Animal(String name, String species, Food food, double amount) {
+    public Animal(String name, String species, int health, int currentHealth, int bite, Food food, double amount) {
         this.name = name;
         this.species = species;
         this.food = food;
         this.amount = amount;
+        this.isfeeded = false;
+        this.health = health;
+        this.currentHealth = currentHealth;
+        this.bite = bite;
+    }
+
+    public void heal() {
+        int healAmount = rand.nextInt(100) + 30;
+        currentHealth = Math.min(currentHealth + healAmount,health);
+        System.out.print(name + "'s life is healed to " + currentHealth + " with " + healAmount + " healing from");
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+
+    public boolean isDead() {
+        return currentHealth <= 0;
+    }
+
+
+    public boolean isIsfeeded() {
+        return isfeeded;
+    }
+
+    public void removeIsFeeded() {
+        isfeeded = false;
     }
 
     public Food getFood() {
@@ -43,5 +83,40 @@ public class Animal {
     public void printAnimal() {
         System.out.println("│       ├── Animal: " + name + ", " + species);
     }
+
+    public void feed() {
+        if (!isfeeded) {
+            System.out.println(Colors.COLORS[2] + name + " (" + species + ") is fed" + Colors.RESET);
+            isfeeded = true;
+        } else {
+            System.out.println(Colors.COLORS[1] + name + " (" + species + ") is already fed." + Colors.RESET);
+        }
+    }
+
+    public void attack(Animal victom){
+        victom.currentHealth -= bite;
+        System.out.println(Colors.COLORS[5] + name + " bites " + victom.name + " (Life of " + victom.name + " " + victom.currentHealth + ")" + Colors.RESET);
+        if (victom.isDead()) {
+            System.out.println(Colors.COLORS[1] + victom.name + " is dead. " + Colors.RESET);
+        }
+    }
+
+    public Animal findLowestHealthAnimal(Animal bestChoice){
+        if (bestChoice == null){
+            bestChoice = this;
+        } else {
+            if (currentHealth/(double)health < bestChoice.currentHealth/(double)bestChoice.health){
+                System.out.printf("%s [%.2f %%] replaces as best choice %s [%.2f %%]\n",
+                        name,
+                        100*currentHealth/(double)health,
+                        bestChoice.name,
+                        100*bestChoice.currentHealth/(double)bestChoice.health);
+                bestChoice = this;
+            }
+        }
+        return bestChoice;
+    }
+
 }
+
 
--- src/main/java/benjamin/week09/zooSimulation/Zoo.java
+++ src/main/java/benjamin/week09/zooSimulation/Zoo.java
@@ -1,34 +1,92 @@
 package benjamin.week09.zooSimulation;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Vector;
 
 public class Zoo {
+    private static Random rand = new Random();
     private String name;
-    private String foundingYear;
+    private int foundingYear;
     private Vector<Enclosure> enclosureList;
+    private Vector<Keeper> keeperList;
+    private Vector<Doctor> doctorList;
 
-
-    public Zoo(String name, String foundingYear) {
+    public Zoo(String name, int foundingYear) {
         this.name = name;
         this.foundingYear = foundingYear;
         this.enclosureList = new Vector<>();
+        this.keeperList = new Vector<>();
+        this.doctorList = new Vector<>();
+    }
+
+    public void addDoctor(Doctor doctor){
+        doctorList.add(doctor);
+    }
+
+    public void removeDoctor(Doctor doctor){
+        doctorList.remove(doctor);
     }
 
     public String getName() {
         return name;
     }
 
-    public Vector<Enclosure> getEnclosureList() {
-        return enclosureList;
-    }
-
     public void addEnclosure(Enclosure enclosure) {
-        enclosureList.add(enclosure);
+        if (!enclosureList.contains(enclosure)) {
+            enclosureList.add(enclosure);
+        }
     }
 
     public void removeEnclosure(Enclosure enclosure) {
         enclosureList.remove(enclosure);
+    }
+
+    public void addKeeper(Keeper keeper) {
+        if (!keeperList.contains(keeper)) {
+            keeperList.add(keeper);
+        }
+    }
+
+    public void simulate() {
+        /* Reset isProcessed Flag */
+        for (Enclosure enc : enclosureList) {
+            enc.removeIsProcessed();
+        }
+        Collections.shuffle(keeperList);
+        for (Keeper keeper : keeperList) {
+            keeper.simulate();
+        }
+        for (Enclosure enc : enclosureList) {
+            enc.simulate();
+        }
+        for (Doctor doc: doctorList) {
+                doc.work(this);
+            }
+    }
+
+    public Animal findOverall() {
+        Animal overallLowest = null;
+        for (Enclosure enclosure : enclosureList) {
+            overallLowest = enclosure.findLowestHealthAnimal(overallLowest);
+        }
+            /*
+            Animal enclosureLowest = enclosure.findLowestHealthAnimal();
+            if(enclosureLowest == null){
+                continue;
+            }
+            if (enclosureLowest.getCurrentHealth() == enclosureLowest.getHealth()) {
+                continue;
+            }
+            if (overallLowest == null){
+                overallLowest = enclosureLowest;
+            } else if (enclosureLowest.getHealthPercent() < overallLowest.getHealthPercent()) {
+                overallLowest = enclosureLowest;
+            }
+        }
+        */
+        return overallLowest;
     }
 
     public void printZoo() {
@@ -36,11 +94,16 @@ public class Zoo {
         for (Enclosure enclosure : enclosureList) {
             enclosure.printEnclosure();
         }
+        System.out.println("│  ");
+        System.out.println("│├── KeeperList: ");
+        for (Keeper keeper : keeperList) {
+            keeper.printKeeper();
+        }
     }
 
-    public void printFoodRequirement(){
+    public void printFoodRequirement() {
         HashMap<Food, Double> foodRequirement = new HashMap<>();
-        for (Enclosure enclosure: enclosureList) {
+        for (Enclosure enclosure : enclosureList) {
             enclosure.addFoodRequirement(foodRequirement);
         }
         System.out.println();
@@ -50,8 +113,9 @@ public class Zoo {
             double amount = foodRequirement.get(food);
             double price = food.getUnitPrice() * amount;
             cost += price;
-            System.out.println("Food: " + food.getName() + " Amount: " + amount + " Price: " + price + " €");
+            System.out.println("Food: " + food.getName() + ", Amount: " + amount + " " + food.getUnit() + ", Price: " + price + " €");
         }
         System.out.println("Total costs: " + cost + " €");
+        System.out.println();
     }
 }
