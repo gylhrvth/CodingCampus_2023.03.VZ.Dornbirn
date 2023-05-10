2c4065839bd7ad94cfb0dcbf58b50096e1b1ab24
Eric G <eric.gangloff08@gmail.com>
2023-05-10 10:48:25 +0200

    bitteschön hassan

--- src/main/java/hassan/week10/Guest.java
+++ src/main/java/hassan/week10/Guest.java
@@ -1,4 +1,19 @@
 package hassan.week10;
 
 public class Guest {
+    private String reservationName;
+    private int groupSize;
+
+    public Guest(String reservationName, int groupSize) {
+        this.reservationName = reservationName;
+        this.groupSize = groupSize;
+    }
+
+    public String getReservationName() {
+        return reservationName;
+    }
+
+    public int getGroupSize() {
+        return groupSize;
+    }
 }
